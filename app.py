a2c8f9e61fe4857d4a3a5bed2418a3501c6d1a99
Wangjingyi-917 <74671441+Wangjingyi-917@users.noreply.github.com>
2023-09-21 22:19:14 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import flask
+from flask import Flask, render_template
+#from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__) # create app
+app.config.from_object('config.DevelopmentConfig') # define server
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+#db = SQLAlchemy(app) # connect database
+
+#from models import *
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template("index.html")
+
+@app.route('/design-projects/', methods=['GET'])
+def de_projects():
+    return render_template("design-projects.html")
+
+@app.route('/research-projects/', methods=['GET'])
+def re_projects():
+    return render_template("research-projects.html")
+
+@app.route('/members/', methods=['GET'])
+def members():
+    return render_template("members.html")
+
+@app.route('/project1/', methods=['GET'])
+def project1():
+    return render_template("project1.html")
+
+@app.route('/project2/', methods=['GET'])
+def project2():
+    return render_template("project2.html")
+
+@app.route('/project3/', methods=['GET'])
+def project3():
+    return render_template("project3.html")
+    
+
+if __name__ == '__main__':
+    app.run()
